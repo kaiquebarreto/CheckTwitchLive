361c611f3d9d8973c12b2d68cc49e18132e82d38
Kaique Barreto <51871012+kaiquebarreto@users.noreply.github.com>
2021-01-15 00:39:21 -0300

    Versão Inicial

---
+++ script.php
@@ -0,0 +1,41 @@
+<?php 
+
+/*
+    Criado por Kaique Barreto 
+    kaiquebarreto.com | @kaique_barreto 
+*/
+
+$streamer = "yoda"; // Digite o canal do seu Streamer
+$cliente_id = ""; // Digite o seu id, para conseguir um acesse: https://dev.twitch.tv/console/apps
+
+function file_get_contents_curl($url) {
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);     
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Client-ID: $cliente_id",        
+        "Authorization: Bearer **************", 
+        "Accept: application/vnd.twitchtv.v5+json"     
+    ));
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+
+}
+
+$url = "https://api.twitch.tv/$streamer/streams/";
+$json_array = json_decode(file_get_contents_curl($url), true);
+
+// Mostra se a live esta online ou offline
+if (!empty($json_array)) {
+    echo "Live Online"; 
+} else {
+	echo "Live Offline";
+}
+
+?>
